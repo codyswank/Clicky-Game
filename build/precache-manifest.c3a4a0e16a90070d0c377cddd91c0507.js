38bfb3ffeb75fa5966c182acf25a1e0d7d8f1915
Cody Swank <cody.swank1@gmail.com>
2019-06-05 20:27:41 -0400

    created app

---
+++ build/precache-manifest.c3a4a0e16a90070d0c377cddd91c0507.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f06b784b9f3a549babd87d170ebc00df",
+    "url": "/Clicky-Game/index.html"
+  },
+  {
+    "revision": "ad25d0e2f6f2e43515d3",
+    "url": "/Clicky-Game/static/css/main.8b555475.chunk.css"
+  },
+  {
+    "revision": "681b51057d74cfa8975d",
+    "url": "/Clicky-Game/static/js/2.65aa1cca.chunk.js"
+  },
+  {
+    "revision": "ad25d0e2f6f2e43515d3",
+    "url": "/Clicky-Game/static/js/main.93c92820.chunk.js"
+  },
+  {
+    "revision": "fab82da4d9420c34bc57",
+    "url": "/Clicky-Game/static/js/runtime~main.012bace8.js"
+  },
+  {
+    "revision": "ddad5775a50715305470929fa27e9f97",
+    "url": "/Clicky-Game/static/media/char.ddad5775.gif"
+  },
+  {
+    "revision": "8b555717db54c4a996fc1993006ef615",
+    "url": "/Clicky-Game/static/media/cheese.8b555717.gif"
+  },
+  {
+    "revision": "3b2bbc6d756d6991851bf7226a22d096",
+    "url": "/Clicky-Game/static/media/courage.3b2bbc6d.gif"
+  },
+  {
+    "revision": "0ca2c1ee0ef418335695651855681e0e",
+    "url": "/Clicky-Game/static/media/kenny.0ca2c1ee.gif"
+  },
+  {
+    "revision": "dc037c084a2a98bbc49379c7382fa5f7",
+    "url": "/Clicky-Game/static/media/peter.dc037c08.gif"
+  },
+  {
+    "revision": "6659105888e209f428a899a07f179004",
+    "url": "/Clicky-Game/static/media/pika.66591058.gif"
+  },
+  {
+    "revision": "08aae8c0b781e9b8f087281dfd1026be",
+    "url": "/Clicky-Game/static/media/scooby.08aae8c0.gif"
+  },
+  {
+    "revision": "240863ae36e7d7de01744228303910ca",
+    "url": "/Clicky-Game/static/media/sponge.240863ae.gif"
+  }
+]);
